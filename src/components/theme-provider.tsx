074d51edf80beb6d862692fdde9849953aa1823e
NITHISH S <nithish@NITHISHs-MacBook-Air.local>
2026-02-11 23:50:11 +0530

    second

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,13 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+type Props = React.ComponentProps<typeof NextThemesProvider>
+
+export function ThemeProvider({ children, ...props }: Props) {
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  )
 }
